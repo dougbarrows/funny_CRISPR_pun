8c8b41137b1f1be1717e1afe39c0bcf7236e76a0
info <info@info13.cshl.edu>
2019-10-27 21:54:10 -0400

    integrates Minas 2 functions

--- CrisprRun.py
+++ CrisprRun.py
@@ -1,45 +1,55 @@
 #!/usr/bin/env python3
 
-# requires 2 inputs (gene name, species)
+# The main shell for generating a list of transcript start sites for a given CAS, gene, and species. The current version does the following:
+# -- Requires 2 inputs (gene name, species)
+# -- Checks to see if a TSS for that species exists. If the TSS exists, the existing file is passed to a program that identifies the TSS.
+# -- If the TSS does not exist, the Ensembl database is accessed for that species and a new TSS file is generated and then added to the database.
+# -- Once the input .bed file is identified containing the TSSs, the file is interesected with the gene list and yields a new .bed file
+# -- The next module takes the new .bed file (a list of TSS for each gene) and passes it to a module that identifies the gRNAs for each gene
 
 import os, sys, re, glob, shutil
 import subprocess
 import gzip
+from gff3_to_TSSbed import gff3_to_TSSbed
+from genelist_to_TSS_bed import TSSs_of_interest_bed
 
 usage = '\n\n\tusage: {} \n\n\n'
 
-if len(sys.argv) < 3:
+# Initial command line checks. The current version allows 2 inputs: gene name (this can be a single gene entered at the command line or a list of genes in a .txt file), species (this should be entered in lower case and using an underscore for any spaces -- consistent with the Ensembl file)
+varIns = 3
+
+if len(sys.argv) < varIns: 				# check number of input files for too few
 	print('Input files are too few!')
 	sys.stderr.write(usage)
 	sys.exit(1)
-if len(sys.argv) > 3:
+if len(sys.argv) > varIns:					# check for too many input files, which may be caused by uses of spaces in the species
 	print('Too many input files. Or you may have a space in one of your entries. Replace the space with an underscore.')
 	sys.stderr.write(usage)
 	sys.exit(1)
 
-if re.search(r'\s', sys.argv[1]) and len(sys.argv > 3):
+if re.search(r'\s', sys.argv[1]) and len(sys.argv > varIns):
 	print('Your gene name can not contain spaces. If a space is needed, please try an underscore (\'_\').')
 	sys.stderr.write(usage)	
 	sys.exit(1)
-if re.search(r'\s', sys.argv[2]) and len(sys.argv > 3):
+if re.search(r'\s', sys.argv[2]) and len(sys.argv > varIns):
 	print('Your species name can not contain spaces. If a space is needed, please use an underscore (e.g., \'homo_sapiens\').')
 	sys.stderr.write(usage)	
 	sys.exit(1)
 
 def main():
 
-	# capture command line argument
-	geneName = sys.argv[1]
+	# capture command line arguments
+	geneNames = sys.argv[1]
 	speciesName = sys.argv[2]
 
 	currPath = os.getcwd()
-	currFile = currPath + '/' + speciesName + '.bed'	
-	if os.path.isfile(currFile):
+	currFile = currPath + '/' + speciesName	
+	if os.path.exists(currFile):
 		print('A reference file already exists for your species. Is it OK to use that file?')
-		userIn1 = input('yes/no')
+		userIn1 = input('yes/no: ')
 		if userIn1 == 'yes':
 			#this is where we will tag for input to Doug/John's function
-			print('The current file to be used is:', currFile)
+			print('The current file to be used is in:', currFile)
 		else:
 			print('This program does not yet have the capability for an external user to upload their own reference file. Sorry!')
 			sys.exit(0)
@@ -71,8 +81,24 @@ def main():
 					with gzip.open(preGz,'rb') as f_in:
 						with open(preGz[:-3], 'wb') as f_out:
 							shutil.copyfileobj(f_in, f_out)
+			newName = preGz[:-3]
 			os.remove(preGz)
-			print('File ready for next step!')
+			os.remove('CHECKSUMS')
+			gff3_to_TSSbed(newName)							# this is generating a new .bed file of TSSs for the species -- saved so it can be used again
+			newerName = newName[:-4]+'TSS.bed'
+			os.remove(newName)
+			newPath = currPath + '/' + speciesName
+			os.mkdir(newPath)
+			os.rename(newerName,newPath+'/'+newerName)
+
+	fileLocName = currFile + '/'
+	contents=os.listdir(fileLocName)
+	for files in contents:
+		inLocFile = fileLocName + files
+		TSSs_of_interest_bed(inLocFile, geneNames)	# this is generating a new .bed file of TSSs specific to the desired genes
+		
+		dougInput = files[-3:]+'geneofinterest.bed'
+	 
 	
 	sys.exit(0)
 
